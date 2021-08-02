60bf28324c06cd4d47263c0fe72a1cbafec5eb27
s770am <s770am@gmail.com>
2021-08-02 08:28:57 -0500

    add validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
     has_many :reviews
+
+    validates :name, presence: true
+    validates :price, numericality: {greater_than: 0}
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
     belongs_to :product
+
+    validates :review_rating, numericality: {less_than: 10, greater_than: 0}
+    validates :review_text, :user, presence: true
+
 end
