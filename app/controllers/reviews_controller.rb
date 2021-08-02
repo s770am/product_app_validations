ecf21466cfc3e906ff3e3105b7833719f7197396
s770am <s770am@gmail.com>
2021-08-02 07:47:43 -0500

    add delete

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,39 @@
 class ReviewsController < ApplicationController
     
     def new
-        
+        @product = get_product
+@review = @product.reviews.build
+
     end
 
     def create
-        
+        @product = get_product
+        @review = @product.reviews.build(review_params)
+        if @review.save
+            redirect_to product_url(@product)
+        else
+            render :new
+        end
     end
-
-    def show
-        
-    end
-
 
     def destroy
-        
+        @product = get_product
+        @review = Review.find(params[:id])
+
+        if @review.delete
+            redirect_to product_url(@product)
+        else
+            flash.now[:alert] = "there was a problem"
+            redirect_to product_url(@product)
+        end
+    end
+
+    private
+    def get_product
+        @product = Product.find(params[:product_id])
+    end
+
+    def review_params
+        params.require(:review).permit(:user, :review_text, :review_rating)
     end
 end
