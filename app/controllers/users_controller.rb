060f8b06aaaf64ed7fcb25383926939c4b6f7aa0
s770am <s770am@gmail.com>
2021-08-05 07:33:36 -0500

    add create options for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
     def new
-        
+        @user = User.new
     end
 
     def create
-        
+        @user = User.new(user_params)
+
+        if @user.save 
+            redirect_to root_url
+        else
+            render :new
+        end
     end
 
     def edit
@@ -14,4 +20,10 @@ class UsersController < ApplicationController
     def update
         
     end
+
+    private
+def user_params
+    params.require(:user).permit(:email, :password, :user_name)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "products#index"
+  resource :users, only: %i(new create update edit)
   resources :products do
     resources :reviews, except: [:update, :edit, :index, :show]
-    resource :users, only %i(new create update edit)
     end
 end
